4beebf304a69388162634186605fc46e5ff4df8d
adora <ao311@student.le.ac.uk>
2025-12-18 16:56:02 +0000

    User repo to read users from  the text file

---
+++ src/Repo/user_repo.py
@@ -0,0 +1,26 @@
+from user import User
+class UserRepo:
+    def __init__(self, filename):
+        self.filename = filename
+
+        try:
+            open(self.filename, "r").close()
+        except FileNotFoundError:
+            open(self.filename, "w").close()
+
+    def load_users(self):
+        users = []
+        with open(self.filename, "r") as file:
+            for line in file:
+                if line.strip():
+                    username, password, role = line.strip().split(":")
+                    users.append(User(username, password, role))
+
+        return users
+
+
+    def get_user(self, username):
+        for user in self.load_users():
+            if user.username == username:
+                return user
+            return None
