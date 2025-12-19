497e9e4d1d9d80558830a7d3bac5eb511a39e0e4
adora <ao311@student.le.ac.uk>
2025-12-19 15:13:25 +0000

    Backbone for products - main class

---
+++ src/model/product.py
@@ -0,0 +1,7 @@
+class Product:
+    def __init__(self,sku, name, description,quantity, price):
+        self.sku = sku
+        self.name = name
+        self.description = description
+        self.quantity = quantity
+        self.price = price
