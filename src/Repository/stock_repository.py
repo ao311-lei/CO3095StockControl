6c946f67e03035321d59455d8d7fe9bcffb1ca0c
adora <ao311@student.le.ac.uk>
2025-12-20 00:57:29 +0000

    Backbone for stock - main class

---
+++ src/Repository/stock_repository.py
@@ -0,0 +1,18 @@
+class StockRepository:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def load_products(self):
+        pass
+
+    def save_product(self, product):
+        pass
+
+    def get_product(self,sku):
+        pass
+
+    def update_product(self,product):
+        pass
+
+    def delete_product(self,sku):
+        pass
