75e2e4b373ad8b506491fbc178a66b0f213acb17
adora <ao311@student.le.ac.uk>
2025-12-21 21:43:06 +0000

    Login features and sign up features

--- src/service/auth_service.py
+++ src/service/auth_service.py
@@ -1,13 +1,29 @@
+import hashlib
+from user import User
+
 class AuthService:
-    def __init__(self, user_repository):
-        self.user_repository = user_repository
+    def __init__(self, user_repo):
+        self.user_repo = user_repo
         self.current_user = None
 
-    def sign_up(self, username, password, role = "staff"):
-        pass
+    def _hash_password(self, password):
+        return hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+    def sign_up(self, username, password, role= "staff"):
+        if username.strip() == "":
+            raise ValueError("Username cannot be empty")
+
+        if len(password) < 8:
+            raise ValueError("Password must be at least 8 characters")
+
+        if self.user_repo.user_exists(username) is not None:
+            raise ValueError("Username already exists")
+
 
     def login(self, username, password):
-        pass
+        user = self.user_repo.find_by_username(username)
+        if user is None:
+            raise ValueError("User not found")
 
     def logout(self):
         pass
