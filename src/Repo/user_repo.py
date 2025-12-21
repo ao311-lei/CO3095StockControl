feb5c591cb66fa2a62ec6b058081a8c6b868c67e
adora <ao311@student.le.ac.uk>
2025-12-21 10:15:24 +0000

    To save new users to the file

--- src/Repo/user_repo.py
+++ src/Repo/user_repo.py
@@ -24,3 +24,10 @@ class UserRepo:
             if user.username == username:
                 return user
             return None
+
+    def save_user(self, user):
+        if self.get_user(user.username) is not None:
+            raise Exception("User already exists")
+
+        with open(self.filename, "a") as file:
+            file.write(user.username + ":" + user.password + ":" + user.role + "\n")
