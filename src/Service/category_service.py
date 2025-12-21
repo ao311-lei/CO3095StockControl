7d911d769fbc304c3fa6dddc4bc07fe43837c0fd
adora <ao311@student.le.ac.uk>
2025-12-21 23:52:09 +0000

    backbone for category

---
+++ src/Service/category_service.py
@@ -0,0 +1,18 @@
+from category import Category
+
+class CategoryService:
+
+    def __init__(self, category_repo):
+        self.category_repo = category_repo
+
+    def create_category(self, category_id, name):
+        pass
+
+    def rename_category(self, category_id, new_name):
+        pass
+
+    def deactivate_category(self, category_id):
+        pass
+
+    def list_categories(self):
+        pass
