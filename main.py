b7a003fb00e61397dd9ef1d74fe12065b190a3e9
ac957 <ac957@student.le.ac.uk>
2025-12-22 00:55:12 +0000

    Implement products menu navigation (view, search, filter placeholders)

--- main.py
+++ main.py
@@ -26,6 +26,22 @@ def stock_menu(menus, auth_service, stock_service):
             break
         else:
             print("Invalid choice. Try again.")
+            
+def products_menu(menus, product_service):
+    while True:
+        choice = menus.view_products_menu()
+
+        if choice == "1":
+            print("TODO later: View all products")
+        elif choice == "2":
+            print("TODO later: Search products")
+        elif choice == "3":
+            print("TODO later: Filter products")
+        elif choice == "0":
+            break
+        else:
+            print("Invalid choice. Try again.")
+
 
 
 
