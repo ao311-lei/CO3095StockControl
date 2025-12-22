bee13bf5b3890a1f457f184f6c8c5275b0ffa9fb
ac957 <ac957@student.le.ac.uk>
2025-12-22 00:52:40 +0000

    Implement stock menu navigation (increase/decrease placeholders)

--- main.py
+++ main.py
@@ -14,5 +14,20 @@ def auth_menu(menus, auth_service):
         else:
             print("Invalid choice. Try again.")
 
+def stock_menu(menus, auth_service, stock_service):
+    while True:
+        choice = menus.view_stock_menu()
+
+        if choice == "1":
+            print("TODO later: Record stock increase")
+        elif choice == "2":
+            print("TODO later: Record stock decrease")
+        elif choice == "0":
+            break
+        else:
+            print("Invalid choice. Try again.")
+
+
+
 
 
