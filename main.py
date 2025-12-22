6009583265d89197542020bd9e9074c313e2eaf8
adora <ao311@student.le.ac.uk>
2025-12-22 04:12:12 +0000

    main.py changes in line with git changes

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from model.menus import Menus
 from Repo.product_repo import ProductRepo
 from Service.product_service import ProductService
+from Repo.user_repo import UserRepo
+from Repo.stock_repo import StockRepo
+from Service.auth_service import AuthService
+from Service.stock_service import StockService
 
+def main():
+    user_repo = UserRepo("users.txt")
+    stock_repo = StockRepo("stock.txt")
+
+    auth_service = AuthService(user_repo)
+    stock_service = StockService(stock_repo)
+
+    auth_menu(auth_service)
+
+    if auth_service.current_user is not None:
+        stock_menu(auth_service, stock_service)
+
+
+if __name__ == "__main__":
+    main()
 def auth_menu(menus, auth_service):
     while True:
         choice = menus.view_account_menu()
@@ -91,7 +110,6 @@ def main():
 
 if __name__ == "__main__":
     main()
-
 
 
 
