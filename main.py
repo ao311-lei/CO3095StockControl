76792c992cf31e097a0bcb4f5d9fbd619a7fc9a2
ac957 <ac957@student.le.ac.uk>
2025-12-22 00:51:37 +0000

    Implement account menu navigation (login/signup placeholders)

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from model.menus import Menus
+
+
+def auth_menu(menus, auth_service):
+    while True:
+        choice = menus.view_account_menu()
+
+        if choice == "1":
+            print("TODO later: Login")
+        elif choice == "2":
+            print("TODO later: Sign up")
+        elif choice == "0":
+            break
+        else:
+            print("Invalid choice. Try again.")
+
+
 
