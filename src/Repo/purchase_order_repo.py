0b1cd7112c308c9d4c464ceed27721bf4c688cee
adora <ao311@student.le.ac.uk>
2025-12-24 21:45:54 +0000

    purchase order repository with txt persistence

---
+++ src/Repo/purchase_order_repo.py
@@ -0,0 +1,33 @@
+from model.purchase_order import PurchaseOrder, PurchaseOrderLine
+
+PO_FILE = "purchase_orders.txt"
+
+class PurchaseOrderRepo:
+    def save_purchase_order(sel, purchase_order,lines):
+        with open(PO_FILE,"w+") as file:
+            file.write(
+                f"{purchase_order.po_id}|"
+                f"{purchase_order.expected_date}|"
+                f"{purchase_order.created_by}|"
+                f"{purchase_order.status}|")
+
+            for line in lines:
+                file.write(
+                    f"{line.po_id}|{line.sku}|{line.quantity}\n"
+                )
+
+    def get_purchase_order(self):
+        orders = []
+
+        try:
+            with open(PO_FILE,"r") as file:
+                for line in file:
+                    parts= line.split("|")
+                    if len(parts) == 5:
+                        orders.append(PurchaseOrder(
+                            parts[0], parts[1], parts[2], parts[3], parts[4]
+                        ))
+        except FileNotFoundError:
+            pass
+
+        return orders
