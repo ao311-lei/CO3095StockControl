36d86496a4dee4d2e1d5c00a69c01435a9c63482
adora <ao311@student.le.ac.uk>
2025-12-28 20:24:34 +0000

    file rewrite

--- src/Repo/purchase_order_repo.py
+++ src/Repo/purchase_order_repo.py
@@ -3,6 +3,22 @@ from model.purchase_order import PurchaseOrder, PurchaseOrderLine
 PO_FILE = "purchase_orders.txt"
 
 class PurchaseOrderRepo:
+
+    def update_po_status(self,po_id,new_status):
+        try:
+            with open(PO_FILE,"r") as file:
+                lines = file.readlines()
+        except FileNotFoundError:
+            return False
+
+        updated = False
+        new_lines = []
+
+        for line in lines:
+            parts = line.strip().split("|")
+
+            if len(parts) == 5 and parts[0] == po_id:
+                parts[4]
     def save_purchase_order(self, purchase_order,lines):
         with open(PO_FILE,"a") as file:
             file.write(
