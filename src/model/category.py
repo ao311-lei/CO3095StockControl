5065b58a2d364d3125681b7ffbc7da2a1bb302d3
adora <ao311@student.le.ac.uk>
2025-12-29 22:45:43 +0000

    Ensuring that the category files are being the source of import

--- src/model/category.py
+++ src/model/category.py
@@ -2,5 +2,5 @@ class Category:
     def __init__(self, category_id, name, active=True):
         self.category_id = category_id
         self.name = name
-        self.active = active\
+        self.active = active
 
--- src/model/product.py
+++ src/model/product.py
@@ -1,4 +1,4 @@
-from unicodedata import category
+from model.category import category
 
 
 class Product:
