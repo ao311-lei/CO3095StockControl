15ef921413eb0f4b54f3f40c8d4b5e5de0f9d9a9
adora <ao311@student.le.ac.uk>
2025-12-31 17:56:45 +0000

    Update user model to default STAFF role and to seperate roles

--- src/model/user.py
+++ src/model/user.py
@@ -1,5 +1,11 @@
 class User:
-    def __init__(self,username,password, role):
+    def __init__(self,username,password, role = "STAFF"):
         self.username = username
         self.password = password
-        self.role = role
+        self.role = role.upper()
+
+    def is_admin(self):
+        return self.role == 'ADMIN'
+
+    def is_manager(self):
+        return self.role in ("MANAGER", "STAFF")
