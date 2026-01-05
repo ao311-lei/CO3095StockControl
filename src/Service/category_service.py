9590e05ebc0eae7ad13463556293431cfcbe53e1
adora <ao311@student.le.ac.uk>
2026-01-05 01:05:26 +0000

    fixing bugs

--- src/Service/category_service.py
+++ src/Service/category_service.py
@@ -1,4 +1,6 @@
+from typing import List
 from model.category import Category
+
 
 class CategoryService:
 
@@ -6,26 +8,24 @@ class CategoryService:
         self.category_repo = category_repo
 
     def create_category(self, category_id, name):
-        if name.strip == "":
+        if name.strip() == "":
             raise ValueError("Category name cannot be empty")
 
         if self.category_repo.get_by_name(name):
             raise ValueError("Category name already exists")
 
-        categories = self.category_repo.get_categories()
-
+        categories: List[Category] = self.category_repo.get_categories()
         categories.append(Category(category_id, name, True))
-
         self.category_repo.save(categories)
 
     def rename_category(self, category_id, new_name):
-        if new_name.strip == "":
+        if new_name.strip() == "":
             raise ValueError("Category name cannot be empty")
 
         if self.category_repo.get_by_name(new_name):
             raise ValueError("Category name already exists")
 
-        categories = self.category_repo.get_categories()
+        categories: List[Category] = self.category_repo.get_categories()
 
         for category in categories:
             if category.category_id == category_id:
@@ -34,8 +34,9 @@ class CategoryService:
                 return category.name
 
         raise ValueError("Category not found")
+
     def deactivate_category(self, category_id):
-        categories = self.category_repo.get_categories()
+        categories: List[Category] = self.category_repo.get_categories()
 
         for category in categories:
             if category.category_id == category_id:
